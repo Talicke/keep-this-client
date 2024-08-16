e8b5d0699c64bc1c85e701a7051cf8b6e862d5f0
Talique <ropars.k@gmail.com>
2024-08-16 16:33:33 +0200

    add footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div className="main">
       <Header/>
+      <Footer/>
     </div>
   );
 }
