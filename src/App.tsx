bf53675ac4706e7346d49605d1ed513ca0268310
Talique <ropars.k@gmail.com>
2024-08-16 17:09:15 +0200

    feat: add registration components and css

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Registration from './Components/Registration';
 
 function App() {
   return (
     <div className="main">
       <Header/>
+      <Registration/>
       <Footer/>
     </div>
   );
